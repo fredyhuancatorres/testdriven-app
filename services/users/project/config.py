8f46b4f24cc8d18d941866f1dbf3aedcafc7272c
abelthf <abel.huanca@upeu.edu.pe>
2023-01-05 22:33:02 -0500

    add jwt setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -8,10 +8,12 @@ class BaseConfig:
     DEBUG = False  # new
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'  # new
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # new
     DEBUG_TB_ENABLED = False              # new
     DEBUG_TB_INTERCEPT_REDIRECTS = False  # new
     BCRYPT_LOG_ROUNDS = 13  # new
+    TOKEN_EXPIRATION_DAYS = 30    # new
+    TOKEN_EXPIRATION_SECONDS = 0  # new
 
 
 class DevelopmentConfig(BaseConfig):
@@ -26,6 +28,8 @@ class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")  # new
     BCRYPT_LOG_ROUNDS = 4  # new
+    TOKEN_EXPIRATION_DAYS = 0     # new
+    TOKEN_EXPIRATION_SECONDS = 3  # new
 
 
 class ProductionConfig(BaseConfig):
